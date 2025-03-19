4d6950ac0d5b0a110a5307226143f3e5ce1db885
Khoa Hoang Hoa <khoa.hoanghoa@nashtechglobal.com>
2025-03-19 15:22:05 +0700

    Add typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { JSX } from "react/jsx-runtime";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, React + TypeScript with Vite! 🚀</h1>
+    </div>
+  );
+};
+
+export default App;
