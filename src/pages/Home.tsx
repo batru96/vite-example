d4751b3f67d0d4eda2d8e98e2327e6e4810d1e88
Khoa Hoang Hoa <khoa.hoanghoa@nashtechglobal.com>
2025-03-20 13:35:21 +0700

    Test disconnect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home: React.FC = () => {
-  return <h1 className="text-3xl font-bold">🏠 Home Page 1.0</h1>;
+  return <h1 className="text-3xl font-bold">🏠 Home Page 1.1</h1>;
 };
 
 export default Home;
